dd356ed165cf149b5c834010e5c0e445d754b9ff
Kole Lam <kolelamied1@gmail.com>
2023-03-13 05:58:15 -0600

    updated let list

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,10 @@
 // Assignment Code; let is more modern version of var
 let generateBtn = document.querySelector("#generate");
 let length = document.querySelector('#length');
-
+let passlength = document.querySelector('#lengthresult')
+let numbers = document.querySelector('#numbers');
+let symbols = document.querySelector('#symbols');
+let copypassword = document.querySelector('#copy');
 
 // Write password to the #password input
 function writePassword() {
